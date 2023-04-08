f90c3a31357056025f2f16d4c809721c1f29f2b5
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2023-04-08 19:37:49 +0100

    Header component implemented & CSS adjustments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return <div>App</div>
+const App = () => {
+  return (
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div>
+          <Header />
+        </div>
+      </div>
+    </div>
+    );
 }
 
 export default App;
